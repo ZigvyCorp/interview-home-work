ba5b6d644efb2f361a5d0eeafb10a1ce05936528
Ho Nam <hsnam.it@gmail.com>
2019-12-23 18:12:34 +0700

    Update UI after login successfully

--- src/frontend/dummy-social-club/src/components/common/Header.js
+++ src/frontend/dummy-social-club/src/components/common/Header.js
@@ -2,20 +2,36 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Header = () => {
-  return (
-    <nav className="navbar navbar-inverse">
-      <div className="container-fluid">
-        <div className="navbar-header">
-          <a className="navbar-brand" href="/">Dummy Social Club</a>
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+    }
+  }
+  componentDidMount() {
+    const user = localStorage.getItem('user');
+    if (user) this.setState({ username: user });
+  }
+
+  render() {
+    const loginPageElement = <NavLink to="/login" exact><span className="glyphicon glyphicon-user"></span> Login</NavLink>;
+    const detailPageElement = <NavLink to="/userdetail" exact><span className="glyphicon glyphicon-user"></span> {this.state.username}</NavLink>;
+    const targetElement = this.state.username.length > 0? detailPageElement: loginPageElement;
+    return (
+      <nav className="navbar navbar-inverse">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <a className="navbar-brand" href="/">Dummy Social Club</a>
+          </div>
+          <ul className="nav navbar-nav navbar-right">
+            {targetElement}
+          </ul>
         </div>
-        <ul className="nav navbar-nav navbar-right">
-          <NavLink to="/userdetail" exact><span className="glyphicon glyphicon-user"></span> Username</NavLink>
-          <NavLink to="/login" exact><span className="glyphicon glyphicon-user"></span> Login</NavLink>
-        </ul>
-      </div>
-    </nav>
-  );
+      </nav>
+    )
+  };
 };
 
 export default Header;
---
+++ src/frontend/dummy-social-club/src/components/common/NewPost.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewPost = () => {
+  return (
+    <div>
+      <h2>Add new post</h2>
+    </div>
+  )
+}
+
+export default NewPost;
