45cccba570d0c8398cdecc3e7c73b99b856b0ef6
Ted <damquangkhoa02@gmail.com>
2020-01-07 10:43:16 +0700

    Add Layout and Homepage View

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,19 @@
-import React, { Component, Suspense } from "react";
-import {
-  Switch,
-  Route,
-  Redirect,
-  BrowserRouter as Router
-} from "react-router-dom";
+import React, { Component, Suspense } from 'react'
+import { Switch, Route, Redirect, BrowserRouter as Router } from 'react-router-dom'
 
-import routes from "./route";
+import routes from './route'
 
-import { Spin } from "antd";
-import { BlankLayout } from "./common/BlankLayout";
-import { ACCESS_TOKEN } from "./constant/constants";
-import { createStore, applyMiddleware, compose } from "redux";
-import appReducer from "./redux";
-import { Provider } from "react-redux";
-import thunk from "redux-thunk";
+import { Spin } from 'antd'
+import { BlankLayout } from './common/BlankLayout'
+import { ACCESS_TOKEN } from './constant/constants'
 
 const WaitingComponent = (Layout, Component) => props => (
   <Layout>
-    <Suspense fallback={<Spin tip="Loading..." />}>
+    <Suspense fallback={<Spin tip='Loading...' />}>
       <Component {...props} />
     </Suspense>
   </Layout>
-);
+)
 
 const PrivateRoute = ({ isLogin, component: Component, ...rest }) => (
   <Route
@@ -33,22 +24,22 @@ const PrivateRoute = ({ isLogin, component: Component, ...rest }) => (
       ) : (
         <Redirect
           to={{
-            pathname: "/"
+            pathname: '/',
           }}
         />
       )
     }
   />
-);
+)
 
 export default class App extends Component {
   showContent = () => {
-    let result = [];
-    const isLogin = localStorage.getItem(ACCESS_TOKEN);
+    let result = []
+    const isLogin = localStorage.getItem(ACCESS_TOKEN)
 
     if (routes.length > 0) {
       result = routes.map((route, index) => {
-        const layout = route.layout || BlankLayout;
+        const layout = route.layout || BlankLayout
 
         if (route.auth && !isLogin) {
           return (
@@ -59,28 +50,23 @@ export default class App extends Component {
               component={WaitingComponent(layout, route.main)}
               isLogin={isLogin}
             />
-          );
+          )
         }
 
         return (
-          <Route
-            key={index}
-            path={route.path}
-            exact={route.exact}
-            component={WaitingComponent(layout, route.main)}
-          />
-        );
-      });
+          <Route key={index} path={route.path} exact={route.exact} component={WaitingComponent(layout, route.main)} />
+        )
+      })
     }
 
-    return <Switch>{result}</Switch>;
-  };
+    return <Switch>{result}</Switch>
+  }
 
   render() {
     return (
       <Router>
-        <div className="App">{this.showContent()}</div>
+        <div className='App'>{this.showContent()}</div>
       </Router>
-    );
+    )
   }
 }
--- client/src/common/GlobalLayout.jsx
+++ client/src/common/GlobalLayout.jsx
@@ -1,45 +1,122 @@
-import React from "react";
-import { Layout, Menu, Icon } from "antd";
-import { Link } from "react-router-dom";
-const { Header, Content, Footer, Sider } = Layout;
+import React from 'react'
+import { Layout, Menu, Icon, Breadcrumb, Dropdown } from 'antd'
+import { Link } from 'react-router-dom'
+import { Row, Col } from 'antd'
+
+import './GlobalLayout.css'
+const { SubMenu } = Menu
+
+const { Header, Content, Footer, Sider } = Layout
+
+const UserMenu = (
+  <Menu>
+    <Menu.Item key='1'>
+      <Icon type='user' />
+      1st menu item
+    </Menu.Item>
+    <Menu.Item key='2'>
+      <Icon type='user' />
+      2nd menu item
+    </Menu.Item>
+    <Menu.Item key='3'>
+      <Icon type='user' />
+      3rd item
+    </Menu.Item>
+  </Menu>
+)
 
 const GlobalLayOut = props => {
   return (
-    <Layout style={{ minHeight: "100vh" }}>
-      <Sider
-        breakpoint="lg"
-        collapsedWidth="0"
-        onBreakpoint={broken => {
-          console.log(broken);
-        }}
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type);
-        }}
-      >
-        <div className="logo" />
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={["4"]}>
-          <Menu.Item key="1">
-            <Icon type="user" />
-            <span className="nav-text">Home</span>
-            <Link to={`/`}></Link>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <Icon type="video-camera" />
-            <span className="nav-text">Map</span>
-            <Link to={`/map`}></Link>
-          </Menu.Item>
-        </Menu>
-      </Sider>
+    <Layout className='layout'>
+      <Header className='header'>
+        <Row>
+          <Col span={20}>
+            <div className='logo' />
+            <Menu theme='dark' mode='horizontal' defaultSelectedKeys={['1']} style={{ lineHeight: '64px' }}>
+              <Menu.Item key='1'>
+                Home
+                <Link to={`/`}></Link>
+              </Menu.Item>
+              <Menu.Item key='2'>
+                New Post
+                <Link to={`/map`}></Link>
+              </Menu.Item>
+              <Menu.Item key='3'>nav 3</Menu.Item>
+            </Menu>
+          </Col>
+          <Col span={4}>
+            <Dropdown.Button type='link' overlay={UserMenu} icon={<Icon type='user' />}>
+              Ted Mosby
+            </Dropdown.Button>
+          </Col>
+        </Row>
+      </Header>
       <Layout>
-        <Header style={{ background: "#fff", padding: 0 }} />
-        <Content style={{ margin: "24px 16px 0" }}>
-          <div style={{ background: "#fff", minHeight: "100vh" }}>
+        <Sider width={200} style={{ background: '#fff' }}>
+          <Menu
+            mode='inline'
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            style={{ height: '100%', borderRight: 0 }}
+          >
+            <SubMenu
+              key='sub1'
+              title={
+                <span>
+                  <Icon type='user' />
+                  2019
+                </span>
+              }
+            >
+              <Menu.Item key='1'>Công nghệ </Menu.Item>
+              <Menu.Item key='2'>Văn hoá</Menu.Item>
+              <Menu.Item key='3'>Nghệ thuật</Menu.Item>
+              <Menu.Item key='4'>Ẩm thực</Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key='sub2'
+              title={
+                <span>
+                  <Icon type='laptop' />
+                  2018
+                </span>
+              }
+            ></SubMenu>
+            <SubMenu
+              key='sub3'
+              title={
+                <span>
+                  <Icon type='notification' />
+                  2017
+                </span>
+              }
+            >
+              <Menu.Item key='9'>option9</Menu.Item>
+              <Menu.Item key='10'>option10</Menu.Item>
+              <Menu.Item key='11'>option11</Menu.Item>
+              <Menu.Item key='12'>option12</Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Sider>
+        <Layout style={{ padding: '0 24px 24px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <Content
+            style={{
+              background: '#fff',
+              padding: 24,
+              margin: 0,
+              minHeight: 280,
+            }}
+          >
             {props.children}
-          </div>
-        </Content>
-        <Footer style={{ textAlign: "center" }}></Footer>
+          </Content>
+        </Layout>
       </Layout>
     </Layout>
-  );
-};
-export default GlobalLayOut;
+  )
+}
+export default GlobalLayOut
