5dec7900b568223453002eaac67a00f64d1b1ad6
thaonguyen2696 <phuongthaonguyen2696@gmail.com>
2020-02-28 11:24:35 +0700

    add sider

--- my-project/frontend/src/components/sider/index.js
+++ my-project/frontend/src/components/sider/index.js
@@ -34,22 +34,22 @@ export default class Sider extends Component {
           key="sub1"
           title={
             <span>
-              <Icon type="mail" />
-              <span>Navigation One</span>
+              <Icon type="setting" />
+              <span>Setting</span>
             </span>
           }
         >
-          <Menu.Item key="1">Option 1</Menu.Item>
-          <Menu.Item key="2">Option 2</Menu.Item>
-          <Menu.Item key="3">Option 3</Menu.Item>
-          <Menu.Item key="4">Option 4</Menu.Item>
+          <Menu.Item key="1">Personal Information</Menu.Item>
+          <Menu.Item key="2">Security</Menu.Item>
+          <Menu.Item key="3">Privacy setting</Menu.Item>
+          <Menu.Item key="4">Public Post</Menu.Item>
         </SubMenu>
         <SubMenu
           key="sub2"
           title={
             <span>
               <Icon type="appstore" />
-              <span>Navigation Two</span>
+              <span>Friends</span>
             </span>
           }
         >
@@ -65,7 +65,7 @@ export default class Sider extends Component {
           title={
             <span>
               <Icon type="setting" />
-              <span>Navigation Three</span>
+              <span>Events</span>
             </span>
           }
         >
