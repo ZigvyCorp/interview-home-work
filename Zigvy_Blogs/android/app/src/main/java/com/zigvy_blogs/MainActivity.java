512d1df0df2cd8161317f6f474d66a08bc8d197f
Kim Tai <letruongkimtai@gmail.com>
2020-05-07 22:23:49 +0700

    initial project

---
+++ Zigvy_Blogs/android/app/src/main/java/com/zigvy_blogs/MainActivity.java
@@ -0,0 +1,15 @@
+package com.zigvy_blogs;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "Zigvy_Blogs";
+  }
+}
