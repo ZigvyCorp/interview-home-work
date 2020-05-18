f8b20dd96ad8b2a6676e359359d0bdab624d1830
Huy-dhk <khanhhuy130295@gmail.com>
2020-05-18 19:18:37 +0700

    update action

--- src/redux/action/action.js
+++ src/redux/action/action.js
@@ -16,13 +16,42 @@ const url =
     ? "/api/"
     : "http://localhost:5000/api/";
 
-export const loadPost = () => {
-  axios.get(`${url}post`).then((result) => {
-    return {
-      type: LOAD_POSTS,
-      listPost: result.length > 0 ? result : [],
-    };
-  }).catch(err=>{
-      console.log(err);
-  });
-};
+export function loadPost() {
+  return (dispatch) => {
+    axios
+      .get(`${url}post`)
+      .then((res) => {
+        let posts = res.data;
+        // let payload ={ listPost: res.length > 0 ? res : []};
+        dispatch({
+          type: LOAD_POSTS,
+          payload: {
+            posts: posts,
+          },
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+}
+
+//title, content, ownerId, tags
+export function createPost(obj) {
+  return (dispatch) => {
+    let data = obj;
+    axios
+      .post(`${url}post`, data)
+      .then((res) => {
+        let post = res.data;
+        dispatch({
+          type: CREATE_POST,
+          payload: {post:post}
+        });
+      
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+}
