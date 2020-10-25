24362b0c8e8ca9a93cd274591a87eecfed10d2d2
Hanhleuit <16520341@gm.uit.edu.vn>
2020-10-25 11:19:48 +0700

    fix bug: prevent push post for the first time

--- frontend/src/packages/@homepage/components/Homepage/container.js
+++ frontend/src/packages/@homepage/components/Homepage/container.js
@@ -3,18 +3,23 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 
 import { getPosts } from '../../actions';
-import {selectedKeyword, selectedLimit, selectedPage} from '../../selector';
+import { selectedKeyword, selectedLimit, selectedPage } from '../../selector';
 
 const container = compose(
   hoc((props) => {
     const dispatch = useDispatch();
     const page = useSelector(selectedPage);
     const limit = useSelector(selectedLimit);
-    const keyword = useSelector(selectedKeyword)
+    const keyword = useSelector(selectedKeyword);
 
     useEffect(() => {
+      if (page > 1 && !window.__notIsFirstTime) {
+        window.__notIsFirstTime = true;
+        return;
+      }
+      window.__notIsFirstTime = true;
       dispatch(getPosts({ page, limit, keyword }));
-    }, [dispatch, page, limit,keyword]);
+    }, [dispatch, page, limit, keyword]);
 
     return {
       ...props,
