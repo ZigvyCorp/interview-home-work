05a64658f38c68fe79b85c66bc1a57cf0d00a768
phu26 <44081554+phu26@users.noreply.github.com>
2021-05-12 23:05:11 +0700

    new header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,13 +1,58 @@
 import React from 'react';
 import {  Typography} from '@material-ui/core';
 import useStyle from  './styles';
-export default function Header(){
-    const classes = useStyle();
-    return (
-        <Typography variant="h4" align="center" className={classes.contaner}>
-             blog
-        </Typography>
+import { makeStyles } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+import Link from '@material-ui/core/Link';
 
-    );
+const useStyles = makeStyles((theme) => ({
+    toolbar: {
+      borderBottom: `1px solid ${theme.palette.divider}`,
+    },
+    toolbarTitle: {
+      flex: 1,
+    },
+    toolbarSecondary: {
+      justifyContent: 'space-between',
+      overflowX: 'auto',
+    },
+    toolbarLink: {
+      padding: theme.spacing(1),
+      flexShrink: 0,
+    },
+  }));
+
+export default function Header(props){
+    const classes = useStyles();
+    const { sections, title } = props;
+
+
+    return (
+        <React.Fragment>
+          <Toolbar className={classes.toolbar}>
+            <Button size="small">icon</Button>
+            <Typography
+              component="h2"
+              variant="h5"
+              color="inherit"
+              align="center"
+              noWrap
+              className={classes.toolbarTitle}
+            >
+              Blog
+            </Typography>
+            <IconButton>
+              <SearchIcon />
+            </IconButton>
+            <Button variant="outlined" size="small">
+              Sign in
+            </Button>
+          </Toolbar>
+         
+        </React.Fragment>
+      );
     
 }
