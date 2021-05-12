ab32aa988550e373ecc9d6852c381ec5aad4c75b
phu26 <44081554+phu26@users.noreply.github.com>
2021-05-12 20:13:44 +0700

    complete redcuers for getpost

---
+++ client/src/constant.js
@@ -0,0 +1,7 @@
+export const INIT_STATE = {
+    posts :{
+        isLoading: false,
+        data: [],
+    },
+   
+};
--- client/src/redux/reducers/posts.js
+++ client/src/redux/reducers/posts.js
@@ -1,3 +1,28 @@
-export default function postsReducers(){
-    
+
+import { INIT_STATE } from "../../constant";
+import { createPost, getPosts, getType } from "../actions";
+
+export default function postsReducers(state = INIT_STATE.posts,action){
+ switch(action.type){
+     case getType(getPosts.getPostsRequest) :
+     return{
+         ...state,
+         isLoading:true,
+     };
+     case getType(getPosts.getPostSuccess) :
+        return{
+            ...state,
+            isLoading:false,
+            data: action.payload
+        };
+        case getType(getPosts.getPostFailure ) :
+            return{
+                ...state,
+                isLoading:false,
+               
+            };
+         
+     default:
+     return state;
+ }
 }
