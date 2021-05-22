6ec8f0e1e05d2268103e189a8fc4051caa1e07dc
jamieminh <48439646+jamieminh@users.noreply.github.com>
2021-05-22 20:19:08 +0700

    minor change

--- front-end-test/src/common/Header/index.js
+++ front-end-test/src/common/Header/index.js
@@ -18,7 +18,7 @@ const Header = () => {
             <div className="User d-flex align-items-center">
                 <img src="https://external-preview.redd.it/XpBx1koNRvc-RNhguYzrrVLyi6ZhMjpau6TfEberwK0.jpg?auto=webp&s=eccb5675183341ffba4e15ac630fd8e19f2e6712" 
                     alt="blank user" width="50" height="50" className="rounded"/>
-                <p className="d-none d-sm-block mb-0 ml-1 h5 ">Nat Romanoff</p>
+                <p className="d-none d-sm-block mb-0 ml-1 h5 ">Hue Nguyen</p>
             </div>
 
 
