a5b778e93b5dba3d4a19a13b32c9fe7f38620a43
Thien.Vu <thien.vu@namlongsoft.net>
2021-08-25 08:23:15 +0700

    init commit

---
+++ ReactNativeTest/index.js
@@ -0,0 +1,11 @@
+/**
+ * @format
+ */
+
+import 'react-native-gesture-handler';
+import {AppRegistry, LogBox} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
+LogBox.ignoreAllLogs(true);
