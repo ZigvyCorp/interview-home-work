a5dd5887af07cac4612fbddf1d3fab2596850b97
vtanh1905 <vtanh1905@gmail.com>
2022-05-14 19:02:29 +0700

    Add Reudx

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux'
+
 import Main from './pages';
+import store from './stores'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Main />
+    <Provider store={store}>
+      <Main />
+    </Provider>
   </React.StrictMode>
 );
