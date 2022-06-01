252598a99cc96eae30aecd9aa9be11b732c948ed
Laughing6901 <vandat96tn@gmail.com>
2022-06-01 22:32:55 +0700

    install typeOrm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, PostsModule, CommentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
