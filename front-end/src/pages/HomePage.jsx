1e2d8c159eed5346b5cc97fa6b5c735d2c08364e
hhddang <dangtramthien@gmail.com>
2023-11-03 10:59:53 +0700

    Add: Search bar

--- front-end/src/pages/HomePage.jsx
+++ front-end/src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllPosts } from "../redux/actions";
 import { POSTS } from "../mock-data";
+import Header from "../components/Header";
 
 export default function HomePage() {
   // const dispatch = useDispatch();
@@ -14,10 +15,20 @@ export default function HomePage() {
   // }, [dispatch]);
 
   return (
-    <div className="d-flex flex-column gap-3">
-      {POSTS.map((post, index) => (
-        <PostCard post={post} key={index} />
-      ))}
+    <div>
+      <Header />
+
+      <div className="search-bar container py-2 mb-3">
+        <form action="" className="w-100 text-center">
+          <input type="text" placeholder="Find post" className="w-50" />
+        </form>
+      </div>
+
+      <div className="d-flex flex-column gap-3">
+        {POSTS.map((post, index) => (
+          <PostCard post={post} key={index} />
+        ))}
+      </div>
     </div>
   );
 }
