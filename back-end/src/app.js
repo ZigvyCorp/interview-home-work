e70ebe0631ca723e9ccb96da0adb805aaafc81c2
hhddang <dangtramthien@gmail.com>
2023-11-03 15:26:23 +0700

    Config: MongoDB

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,11 +1,27 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({ message: "success" });
-});
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connect to MongoDB successfully");
+    startServer();
+  })
+  .catch((e) => {
+    console.log("Cannot connect to MongoDB. Error: " + e);
+  });
 
-app.listen(5000, () => {
-  console.log("Server starts at http://localhost:5000");
-});
+const startServer = () => {
+  app.get("/", (req, res) => {
+    res.json({ message: "success" });
+  });
+
+  app.listen(5000, () => {
+    console.log("Server starts at http://localhost:5000");
+  });
+};
