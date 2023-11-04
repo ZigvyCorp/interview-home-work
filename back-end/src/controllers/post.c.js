d158e45b51da2ed072cd87a28264c6e79a021df8
hhddang <dangtramthien@gmail.com>
2023-11-04 09:28:43 +0700

    Update: Post API

--- back-end/src/controllers/post.c.js
+++ back-end/src/controllers/post.c.js
@@ -1,8 +1,8 @@
 import { postModel } from "../models/post.m.js";
-import { userModel } from "../models/user.m.js";
 import { failedResponse, successResponse } from "../utils/response.js";
 
 export const getAllPosts = (req, res) => {
+  const searchTitle = req.query.title;
   return postModel
     .aggregate([
       {
@@ -23,7 +23,11 @@ export const getAllPosts = (req, res) => {
       },
     ])
     .then((posts) => {
-      return res.status(200).json(successResponse({ posts }));
+      const response = posts.filter((post) =>
+        post.title.toLowerCase().includes(searchTitle?.toLowerCase() || "")
+      );
+      // }
+      return res.status(200).json(successResponse({ posts: response }));
     })
     .catch((e) => res.status(500).json(failedResponse(e.message)));
 };
