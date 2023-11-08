34b2b5eacbc1bd825a9d53dda74719f236827d50
quangnguyen <quangwk2702@gmail.com>
2023-11-08 21:00:23 +0700

    setting up controller for paginaiton and search

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -2,10 +2,21 @@ import { Pagination as AntPagination } from "antd";
 
 export interface IPagination {
   className?: string;
+  pageSize: number;
+  total: number;
+  onChange: (page: number, pageSize: number) => void;
 }
 
-const Pagination = ({ className = "" }: IPagination) => {
-  return <AntPagination className={`${className}`} />;
+const Pagination = ({ className = "", pageSize, total, onChange }: IPagination) => {
+  return (
+    <AntPagination
+      className={`${className}`}
+      defaultCurrent={1}
+      pageSize={pageSize}
+      total={total}
+      onChange={onChange}
+    />
+  );
 };
 
 export default Pagination;
--- src/modules/blogs/components/BlogList.tsx
+++ src/modules/blogs/components/BlogList.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
 import { IPost } from "@/modules/blogs";
+import { Pagination } from "@/components/pagination";
 
 export interface IBlogList {
   className?: string;
@@ -11,16 +12,21 @@ export interface IBlogList {
 const BlogList = ({ className = "", blogs }: IBlogList) => {
   const [params, setParams] = useSearchParams();
   const [search, setSearch] = useState<string>("");
+  const [pagination, setPagination] = useState();
 
   return (
     <div className={`${className}`}>
       {blogs.map((item, index) => {
         return (
           <div key={index}>
-            <h2>{item.author}</h2>
+            <h2>{item.title}</h2>
           </div>
         );
       })}
+
+      <div className="pt-[30px]">
+        <Pagination total={blogs.length} pageSize={30} onChange={() => {}} />
+      </div>
     </div>
   );
 };
