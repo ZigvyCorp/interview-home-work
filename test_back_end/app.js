2102391e4a85744fe2699d3d0e4b5ce4a18fd7a5
Amirelo <80700398+Amirelo@users.noreply.github.com>
2023-11-09 22:42:43 +0700

    Change app service to fetch data from expressJS

--- test_back_end/app.js
+++ test_back_end/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const logger = require('morgan');
+const cors = require('cors');
 var path = require('path');
 const dotenv = require('dotenv');
 dotenv.config();
@@ -12,6 +13,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(logger('dev'));
+app.use(cors());
 
 
 // Routes
--- test_front_end/src/services/AppService.js
+++ test_front_end/src/services/AppService.js
@@ -1,9 +1,12 @@
 export const getAllPosts = async () => {
-    return await fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
+    return await fetch('http://localhost:5001/api/posts')
+        .then(response => {
+            return response.json()
+        }
+        )
         .then(json => {
-            console.log(json);
-            return json;
+            console.log('json:', json);
+            return json.result;
         })
         .catch((error => {
             console.log(error);
@@ -23,29 +26,29 @@ export const getUserByID = async (userId) => {
 
 export const getAllComments = async () => {
     return await fetch('https://jsonplaceholder.typicode.com/comments')
-    .then(response => response.json())
-    .then(json => {
-        console.log(json);
-        return json;
-    }).catch(error=> {
-        console.log(error);
-    })
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+            return json;
+        }).catch(error => {
+            console.log(error);
+        })
 }
 
 export const getPostCommentsByID = async (postId) => {
     return await fetch('https://jsonplaceholder.typicode.com/comments')
-    .then(response => response.json())
-    .then(json => {
-        let list = [];
-        console.log(json);
-        json.forEach(element => {
-            if (element.postId === postId){
-                list = [...list, element];
-            }
-        });
-        console.log("Comment list:", list);
-        return list;
-    }).catch(error=> {
-        console.log(error);
-    })
+        .then(response => response.json())
+        .then(json => {
+            let list = [];
+            console.log(json);
+            json.forEach(element => {
+                if (element.postId === postId) {
+                    list = [...list, element];
+                }
+            });
+            console.log("Comment list:", list);
+            return list;
+        }).catch(error => {
+            console.log(error);
+        })
 }
