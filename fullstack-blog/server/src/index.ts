6b1a1a4212d61bb79cb84bace29e4d6a4aa23a31
Nhat Linh <nhatlinh240501@gmail.com>
2023-11-09 10:53:04 +0700

    feat: add middleware

--- fullstack-blog/server/src/index.ts
+++ fullstack-blog/server/src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import withConfig from "./config";
+import { errorMiddleware } from "./middlewares/error";
 
 const app: Express = express();
 const PORT = process.env.PORT || 8000;
@@ -10,6 +11,7 @@ app.use("*", (req: Request, res: Response) => {
 	res.status(400).send("Route not found");
 });
 
+app.use(errorMiddleware);
 app.listen(PORT, () => {
 	console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
