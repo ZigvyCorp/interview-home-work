e5a9f0a47b8ba726624b20fe7e97228c2d108584
Amirelo <80700398+Amirelo@users.noreply.github.com>
2023-11-09 23:21:34 +0700

    Fix ItemComments image uneven scale

--- test_front_end/src/components/ItemComment.js
+++ test_front_end/src/components/ItemComment.js
@@ -1,13 +1,16 @@
 export default function ItemComment({ item }) {
     return (
         <div>
-            <div className="d-inline-flex">
+            <div className="d-inline-flex w-100">
+                <div>
                 <img
-                    className="img-responsive rounded-circle me-4"
+                    className="rounded-circle me-4"
                     src="https://images.pexels.com/photos/18758628/pexels-photo-18758628/free-photo-of-a-highland-cow-on-a-field.jpeg"
                     width={60}
                     height={60} />
-                <div>
+                </div>
+                
+                <div className="">
                     <div className="d-inline-flex">
                         <p className="me-2">{item ? item.name : "Temp name"}</p>
                         <p>2 days ago</p>
