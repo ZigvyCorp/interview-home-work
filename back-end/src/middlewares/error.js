d182973e1fd0a47508d40c5d8ca9c260c9175c70
Thongttdev <thongtt0409@gmail.com>
2023-11-09 19:31:20 +0700

    Zigvy technical assignment test full code

---
+++ back-end/src/middlewares/error.js
@@ -0,0 +1,30 @@
+const ErrorResponse = require('../utils/errorResponse');
+
+const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
+  error.message = err.message;
+
+  if (err.name === 'CastError') {
+    const message = `Resource not found ${err.value}`;
+    error = new ErrorResponse(message, 404);
+  }
+
+  // duplicate record
+  if (err.code === 11000) {
+    const message = 'Duplicate field value entered';
+    error = new ErrorResponse(message, 400);
+  }
+
+  // validation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((val) => '' + val, message);
+    error = new ErrorResponse(message, 400);
+  }
+
+  res.status(error.codeStatus || 500).json({
+    success: false,
+    error: error.message || 'server error',
+  });
+};
+
+module.exports = errorHandler;
