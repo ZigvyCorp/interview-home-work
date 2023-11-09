d3f87bd7ec9b67527b0182f446ad28de13105297
Đinh Quang Tuấn <quangtuan496@gmail.com>
2023-11-09 23:14:30 +0700

    fix port

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const http = require("http");
 
 const app = express();
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
 const usersRoute = require("./routes/users");
 const postsRoute = require("./routes/posts");
