2debcf02fb826301d5a9aa0d50791dd980523e38
dotrong124 <dotrong124@gmail.com>
2023-11-09 15:29:06 +0700

    fix: server mongo

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,16 +8,7 @@ import { DatabaseModule } from "./framework/database-mongodb/database.module";
 import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [
-    CommentModule,
-    PostModule,
-    UserModule,
-    DatabaseModule,
-    MongooseModule.forRoot(
-      "mongodb+srv://doadmin:rR0J35Z71VWio928@db-mongodb-sgp1-76973-464ced0f.mongo.ondigitalocean.com/C300AMG?tls=true&authSource=admin&replicaSet=db-mongodb-sgp1-76973",
-      {}
-    ),
-  ],
+  imports: [CommentModule, PostModule, UserModule, DatabaseModule, MongooseModule.forRoot("mongodb://localhost:27017", {})],
   controllers: [AppController],
   providers: [AppService],
 })
