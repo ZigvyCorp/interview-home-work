b86a34bb2e10cb0425fa7715c95c5befcb78d142
hieuvu <hieu2108@gmail.com>
2023-11-10 12:49:06 +0700

    layout FE

---
+++ BE/index.js
@@ -0,0 +1,13 @@
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const app = express()
+const port = process.env.PORT
+app.use(express.json())
+
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
