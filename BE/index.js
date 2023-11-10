9ab6b79d0b3d8a8f833d774b94c7f2d7ab368e0a
hieuvu <hieu2108@gmail.com>
2023-11-10 19:23:47 +0700

    be

--- BE/index.js
+++ BE/index.js
@@ -1,14 +1,14 @@
 import express from "express"
 import dotenv from "dotenv"
 import routes from "./src/routes/routes.js";
-
+import cors from "cors"
 dotenv.config();
 
 const app = express()
 const port = process.env.PORT
 
 app.use(express.json())
-
+app.use(cors())
 app.use("/api", routes)
 
 app.listen(port, () => {
