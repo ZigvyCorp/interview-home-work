4e19b000c33a33301f3a11e519b067f17d79fa0b
varia <ltnhan2610@gmail.com>
2023-12-10 21:31:18 +0700

    add infinity scroll

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Post from "@/components/Post";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 type BlogPost = {
 	id: number;
@@ -12,8 +12,11 @@ type BlogPost = {
 };
 
 export default function Home() {
+	const batchSize = 5;
 	const [posts, setPosts] = useState([]);
 	const [offset, setOffset] = useState(1);
+	const observerTarget = useRef<HTMLDivElement>(null);
+
 	useEffect(() => {
 		async function fetchPosts() {
 			const res = await fetch(
@@ -24,12 +27,41 @@ export default function Home() {
 				throw new Error("Failed to fetch data");
 			}
 
-			const data = await res.json();
+			let data = await res.json();
+
+			if (data.length > batchSize * offset) {
+				data = data.slice(0, batchSize * offset);
+			} else {
+				if (observerTarget.current) {
+					observerTarget.current.classList.add("d-none");
+				}
+			}
 			setPosts(data);
 		}
 
 		fetchPosts();
-	}, []);
+	}, [offset]);
+
+	useEffect(() => {
+		const observer = new IntersectionObserver(
+			(entries) => {
+				if (entries[0].isIntersecting) {
+					setOffset((prevOffset) => prevOffset + 1);
+				}
+			},
+			{ threshold: 1 }
+		);
+
+		if (observerTarget.current) {
+			observer.observe(observerTarget.current);
+		}
+
+		return () => {
+			if (observerTarget.current) {
+				observer.unobserve(observerTarget.current);
+			}
+		};
+	}, [observerTarget]);
 
 	return (
 		<main className="container mt-3">
@@ -41,6 +73,15 @@ export default function Home() {
 					collapse={true}
 				/>
 			))}
+			<div className="d-flex justify-content-center">
+				<div
+					className="spinner-border"
+					role="status"
+					ref={observerTarget}
+				>
+					<span className="visually-hidden">Loading...</span>
+				</div>
+			</div>
 		</main>
 	);
 }
