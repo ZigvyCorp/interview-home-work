9328d98b3fceb16119e0f63adddc7e228433182d
pham_duc <duc.pham@beetech.com.vn>
2024-02-28 01:50:17 +0700

    Add routes for users, posts, and comments

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,3 +1,6 @@
 module.exports = (app) => {
   app.use("/api/v1/", require("./auth.routes"));
+  app.use("/api/v1/users", require("./users.routes"));
+  app.use("/api/v1/posts", require("./posts.routes"));
+  app.use("/api/v1/comments", require("./comments.routes"));
 };
