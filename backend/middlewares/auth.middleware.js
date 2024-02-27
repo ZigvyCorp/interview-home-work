4af82f466201b79bdc5f8dd6fc104b88be786d1c
pham_duc <duc.pham@beetech.com.vn>
2024-02-28 01:13:40 +0700

    Remove email validation and role check in auth.middleware.js

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -12,22 +12,11 @@ const validateSignUp = [
         }
       });
     }),
-  check("email")
-    .isEmail()
-    .withMessage("Invalid email.")
-    .custom((value) => {
-      return User.findOne({ email: value }).then((user) => {
-        if (user) {
-          return Promise.reject("Email in use");
-        }
-      });
-    }),
   check("password")
     .isLength({ min: 8 })
     .withMessage("Password must be at least 8 characters long")
     .matches(/\d/)
     .withMessage("Password must contain at least one number"),
-  check("role").isIn(["user", "admin"]).withMessage("You must choose a role"),
   check("dob")
     .isISO8601()
     .toDate()
