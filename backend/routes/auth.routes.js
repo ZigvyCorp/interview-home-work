7c2cef9bc5781e67bf5bcdda0dca3a6c081bfe59
pham_duc <duc.pham@beetech.com.vn>
2024-02-27 21:50:21 +0700

    Refactor authentication routes

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,111 +1,19 @@
 const express = require("express");
-const { check, validationResult } = require("express-validator");
-const User = require("../models/user.model");
-const bcrypt = require("bcryptjs");
-const passport = require("passport");
+const { validateSignUp } = require("../middlewares/auth.middleware");
+const {
+  signup,
+  login,
+  logout,
+  loggedin,
+} = require("../controllers/auth.controller");
 const router = express.Router();
 
-const bcryptSalt = 10;
+router.post("/signup", validateSignUp, signup);
 
-router.post(
-  "/signup",
-  [
-    check("username")
-      .isLength({ min: 5 })
-      .withMessage("Username should have min 5 characters.")
-      .custom((value) => {
-        return User.findOne({ username: value }).then((user) => {
-          if (user) {
-            return Promise.reject("The username already exists");
-          }
-        });
-      }),
-    check("email")
-      .isEmail()
-      .withMessage("Invalid email.")
-      .custom((value) => {
-        return User.findOne({ email: value }).then((user) => {
-          if (user) {
-            return Promise.reject("Email in use");
-          }
-        });
-      }),
-    check("password")
-      .isLength({ min: 8 })
-      .withMessage("Password min 8 characters")
-      .matches(/\d/)
-      .withMessage("Password must contain a number"),
-    check("role").isIn(["user", "admin"]).withMessage("You must choose a role"),
-    check("dob")
-      .isISO8601()
-      .toDate()
-      .withMessage("Date of birth must be in ISO 8601 format (YYYY-MM-DD)"),
-  ],
-  (req, res) => {
-    try {
-      const passCheck = validationResult(req);
+router.post("/login", login);
 
-      if (!passCheck.isEmpty()) {
-        res.status(400).json({ message: passCheck.errors });
-        return;
-      }
+router.post("/logout", logout);
 
-      const { username, password, name, email, role, dob } = req.body;
-
-      const salt = bcrypt.genSaltSync(bcryptSalt);
-      const hashPass = bcrypt.hashSync(password, salt);
-
-      User.create({ username, password: hashPass, name, email, role, dob })
-        .then((newUser) =>
-          req.login(newUser, (err) =>
-            err
-              ? res.status(500).json({ message: "Login error" })
-              : res
-                  .status(200)
-                  .json({ message: "Registration successful", user: newUser })
-          )
-        )
-        .catch(() =>
-          res
-            .status(500)
-            .json({ message: "Error saving user to DB. Please try again." })
-        );
-    } catch (error) {}
-  }
-);
-
-router.post("/login", (req, res, next) => {
-  passport.authenticate("local", (err, theUser, failureDetails) => {
-    if (err) {
-      res.status(500).json({ message: "Error authenticating user" });
-      return;
-    }
-    if (!theUser) {
-      res.status(401).json(failureDetails);
-      return;
-    }
-
-    req.login(theUser, (err) =>
-      err
-        ? res.status(500).json({ message: "Session error" })
-        : res.status(200).json(theUser)
-    );
-  })(req, res, next);
-});
-
-router.post("/logout", (req, res) => {
-  req.logout((err) => {
-    if (err) {
-      return res.status(500).json({ message: "Error logging out" });
-    }
-    res.status(200).json({ message: "Log out success!" });
-  });
-});
-
-router.get("/loggedin", (req, res) =>
-  req.isAuthenticated()
-    ? res.status(200).json(req.user)
-    : res.status(403).json({ message: "Unauthorized" })
-);
+router.get("/loggedin", loggedin);
 
 module.exports = router;
