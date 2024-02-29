6b866f3e90731bea1110d62139a9349a4de4cb22
varia <ltnhan2610@gmail.com>
2024-02-29 15:45:15 +0700

    handle submit search form

--- front-end/components/NavBar.tsx
+++ front-end/components/NavBar.tsx
@@ -1,8 +1,20 @@
+"use client";
+
 import Image from "next/image";
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 function NavBar() {
+	const router = useRouter();
+
+	function handleSearch(event: React.FormEvent) {
+		event.preventDefault();
+		const formData = new FormData(event.target as HTMLFormElement);
+		const { keyword } = Object.fromEntries(formData);
+		router.push("/search?keyword=" + keyword);
+	}
+
 	return (
 		<nav className={`navbar bg-body-tertiary sticky-top`}>
 			<div className="container-fluid">
@@ -22,36 +34,39 @@ function NavBar() {
 					</li>
 				</ul>
 				<div className="d-flex" style={{ gap: 15 }}>
-					<div className="input-group">
-						<div className="form-outline" data-mdb-input-init>
-							<input
-								type="search"
-								id="form1"
-								className="form-control"
-							/>
-						</div>
-						<button
-							type="button"
-							className="btn btn-primary"
-							data-mdb-ripple-init
-						>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								strokeWidth={1.5}
-								stroke="currentColor"
-								width={18}
-								height={18}
-							>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+					<form onSubmit={handleSearch}>
+						<div className="input-group">
+							<div className="form-outline" data-mdb-input-init>
+								<input
+									type="search"
+									className="form-control"
+									name="keyword"
 								/>
-							</svg>
-						</button>
-					</div>
+							</div>
+							<button
+								type="submit"
+								className="btn btn-primary"
+								data-mdb-ripple-init
+							>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									strokeWidth={1.5}
+									stroke="currentColor"
+									width={18}
+									height={18}
+								>
+									<path
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+									/>
+								</svg>
+							</button>
+						</div>
+					</form>
+
 					<div className="d-flex gap-2 align-items-center">
 						<Image
 							src="/cat.png"
