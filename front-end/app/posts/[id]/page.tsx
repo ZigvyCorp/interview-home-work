df48f79bddf95e52495940110bd755eb7f5f072c
varia <ltnhan2610@gmail.com>
2024-02-29 21:45:29 +0700

    fix post detail url

--- front-end/app/posts/[id]/page.tsx
+++ front-end/app/posts/[id]/page.tsx
@@ -27,13 +27,13 @@ export default function PostDetails({ params }: Props) {
 		async function fetchPost(id: string) {
 			try {
 				const res = await fetch(
-					`${process.env.NEXT_PUBLIC_BACKEND_URL}/${id}`
+					`${process.env.NEXT_PUBLIC_BACKEND_URL}/posts/${id}`
 				);
 				if (!res.ok) {
 					throw new Error("Failed to fetch data");
 				}
 				const data = await res.json();
-				setPost(data);
+				setPost(data[0]);
 			} catch (error) {
 				console.error(error);
 			}
--- front-end/lib/store.ts
+++ front-end/lib/store.ts
@@ -9,8 +9,7 @@ const persistConfig = {
 	key: "persist",
 	storage,
 };
-// `combineSlices` automatically combines the reducers using
-// their `reducerPath`s, therefore we no longer need to call `combineReducers`.
+
 const rootReducer = combineSlices(counterSlice, quotesApiSlice);
 // Infer the `RootState` type from the root reducer
 export type RootState = ReturnType<typeof rootReducer>;
