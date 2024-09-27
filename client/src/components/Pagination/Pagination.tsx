40f8f56a3565ede1b47a87c93eb222df2ae6da1c
Kim Dien <20520442@gm.uit.edu.vn>
2024-09-27 21:18:48 +0700

    feat: add pagination

---
+++ client/src/components/Pagination/Pagination.tsx
@@ -0,0 +1,71 @@
+import React, { Dispatch, SetStateAction } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  nextPage,
+  pageChange,
+  previousPage,
+} from "../../redux/action/paginationAction";
+import { loading } from "../../redux/action/loadingAction";
+
+export const Pagination = () => {
+  const pageInfo = useSelector((state: {pages: PageInfo}) => state.pages);
+  const totalPages = useSelector((state: PageInfo) => state.totalPages);
+  const dispatch = useDispatch();
+
+  console.log(pageInfo)
+
+  return (
+    <nav aria-label="Page navigation example">
+      <ul className="pagination d-flex justify-content-center">
+        <li className={`page-item ${pageInfo.pageNumber === 1 ? "disabled" : undefined}`}>
+          <button
+            className="page-link"
+            aria-label="Previous"
+            disabled={pageInfo.pageNumber === 1}
+            onClick={() => {
+              dispatch(previousPage());
+              dispatch(loading());
+            }}
+          >
+            <span aria-hidden="true">&laquo;</span>
+          </button>
+        </li>
+        {Array.from(Array(totalPages)).map((item, index) => (
+          <li
+            key={index}
+            className={`page-item ${
+              index + 1 == pageInfo.pageNumber ? "active" : undefined
+            }`}
+          >
+            <button
+              onClick={() => {
+                dispatch(pageChange(index + 1));
+                dispatch(loading());
+              }}
+              className="page-link"
+              disabled={index + 1 === pageInfo.pageNumber}
+            >
+              {index + 1}
+            </button>
+          </li>
+        ))}
+        <li
+          className={`page-item ${
+            pageInfo.pageNumber === totalPages ? "disabled" : undefined
+          }`}
+        >
+          <button
+            className="page-link"
+            disabled={totalPages === pageInfo.pageNumber}
+            onClick={() => {
+              dispatch(nextPage());
+              dispatch(loading());
+            }}
+          >
+            <span aria-hidden="true">&raquo;</span>
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
