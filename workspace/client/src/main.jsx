ec1c049f5eace27d17ef72db28cc6bed332e9c63
Duncan Nguyen <duncan.nguyen.18@gmail.com>
2024-09-28 00:12:38 +0700

    Feature: add redux-persist

--- workspace/client/src/main.jsx
+++ workspace/client/src/main.jsx
@@ -1,15 +1,15 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App.jsx";
 import "./index.css";
-import store from "./redux/store.js";
+import store, { persistor } from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <PersistGate persistor={persistor}>
     <Provider store={store}>
       <App />
     </Provider>
-  </StrictMode>
+  </PersistGate>
 );
--- workspace/client/src/redux/store.js
+++ workspace/client/src/redux/store.js
@@ -1,21 +1,32 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
+import storage from "redux-persist/lib/storage";
+import persistReducer from "redux-persist/es/persistReducer";
 
 import { postsReducer } from "./posts/postsSlice";
 import { commentsReducer } from "./comments/commentsSlice";
 import rootSaga from "./saga";
+import persistStore from "redux-persist/es/persistStore";
 
 const rootReducer = combineReducers({
   posts: postsReducer,
-  comments:commentsReducer
+  comments: commentsReducer,
 });
+const persistedReducer = persistReducer(
+  {
+    key: "root",
+    storage,
+    version: 1,
+  },
+  rootReducer
+);
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(sagaMiddleware),
 });
-
+export const persistor = persistStore(store);
 sagaMiddleware.run(rootSaga);
 export default store;
