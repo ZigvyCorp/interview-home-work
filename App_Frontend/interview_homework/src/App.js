56f45095d7b13fb5338d8d2342b9cf664956f5b2
manduong2k2 <manduong2k2@gmail.com>
2024-09-28 10:25:44 +0700

    update css login

--- App_Frontend/interview_homework/src/App.js
+++ App_Frontend/interview_homework/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Login from './Pages/Login';
+import Login from './Pages/Login/Login';
 import Dashboard from './Pages/Dashboard/Dashboard';
 import CreatePost from './Pages/Post/CreatePost';
 
--- App_Frontend/interview_homework/src/Pages/Login/Login.js
+++ App_Frontend/interview_homework/src/Pages/Login/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import './Login.css'
 
 const Login = () => {
   const [username, setUsername] = useState('');
