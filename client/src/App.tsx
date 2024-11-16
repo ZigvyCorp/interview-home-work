1861a384621bd0f917290fc719630d817a6c4338
annguyenhuu1603 <nguyenhuuan1616@gmail.com>
2024-11-16 12:21:28 +0700

    init project client

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <section>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </section>
+  );
+};
+
+export default App;
